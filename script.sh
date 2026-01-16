0a134be798134b0d70a826115565c1fd9b96d999
Rodrigo Contreras <rg.contreras@duocuc.cl>
2026-01-15 23:52:37 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ echo "RUN pip install flask" >> Dockerfile
 echo "COPY index.html /home/myapp/templates/ " >> Dockerfile
 echo "COPY server.py /home/myapp/ " >> Dockerfile
 echo "EXPOSE 8000 " >> Dockerfile
-echo "CMD ["python3", "/home/myapp/server.py"]
+echo "CMD python3 /home/myapp/server.py
 
 docker build -t webflask .
 docker run -d -p 8000:8000 webflask webflask
