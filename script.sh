91a8f4695d5c9a3fae3ce7a68c23b994e7d43417
Rodrigo Contreras <rg.contreras@duocuc.cl>
2026-01-15 23:51:26 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,11 @@
 echo "FROM python" > Dockerfile
-echo "RUN apt-get update" >> Dockerfile
-echo "RUN apt-get upgrade -y" >> Dockerfile
-echo "RUN apt-get install python3-pip" >> Dockerfile
+echo "RUN apt-get update -y" >> Dockerfile
+echo "RUN apt-get install python3-pip -y" >> Dockerfile
 echo "RUN pip install flask" >> Dockerfile
-echo "COPY index.html /home/myapp/templates/" >> Dockerfile
-echo "COPY server.py /home/myapp/" >> Dockerfile
-echo "EXPOSE 8000" >> Dockerfile
-echo "CMD ["python3", "server.py"]
+echo "COPY index.html /home/myapp/templates/ " >> Dockerfile
+echo "COPY server.py /home/myapp/ " >> Dockerfile
+echo "EXPOSE 8000 " >> Dockerfile
+echo "CMD ["python3", "/home/myapp/server.py"]
 
 docker build -t webflask .
 docker run -d -p 8000:8000 webflask webflask
