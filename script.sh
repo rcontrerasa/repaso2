cc6ab266d2ac7f11c0377b32078686ea3fae28ba
rcontrerasa <rg.contreras@duocuc.cl>
2026-01-15 23:36:59 -0300

    inicio

---
+++ script.sh
@@ -0,0 +1,12 @@
+echo "FROM python" > Dockerfile
+echo "RUN apt-get update" >> Dockerfile
+echo "RUN apt-get upgrade -y" >> Dockerfile
+echo "RUN apt-get install python3-pip" >> Dockerfile
+echo "RUN pip install flask" >> Dockerfile
+echo "COPY index.html /home/myapp/templates/" >> Dockerfile
+echo "COPY server.py /home/myapp/" >> Dockerfile
+echo "EXPOSE 8000" >> Dockerfile
+echo "CMD ['python3', 'server.py']
+
+docker build -t webflask .
+docker run -d -p 8000:8000 webflask webflask
